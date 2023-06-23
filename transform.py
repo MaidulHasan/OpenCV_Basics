d88da1ecd630d93e1bbd172396d38191362a2027
Maidul <u1703016@student.cuet.ac.bd>
2023-06-23 12:35:01 +0600

    [refactor]

--- transform.py
+++ transform.py
@@ -2,13 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import cv2 as cv
-from funcs import matplotlib_imshow
-
-import imutils
+from imutils import rotate_bound
 
 
 def find_contours(img):
-    thresh_val, thresh_img = cv.threshold(img, 120, 255, cv.THRESH_BINARY)
+    thresh_val, thresh_img = cv.threshold(img, 200, 255, cv.THRESH_BINARY)
     cnt, hier = cv.findContours(thresh_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
     return cnt
 
@@ -46,7 +44,7 @@ def rotate(img):
     rotation_angle = np.random.randint(10, 320)
 
     # under the hood, imutils.rotate_bound uses opencv to do this
-    rotated_img = imutils.rotate_bound(img, -rotation_angle)
+    rotated_img = rotate_bound(img, -rotation_angle)
     rotated_img_title = f"Rotation Angle (CCW): {rotation_angle}"
 
     return rotated_img, rotated_img_title
@@ -71,6 +69,18 @@ def transform(
 
         return translated_img_cnt, translated_img_title
 
+    if scale_img:
+        scaled_img, scaled_img_title = scale(img)
+        scaled_img_cnt = find_contours(scaled_img)[0]
+
+        return scaled_img_cnt, scaled_img_title
+
+    if rotate_img:
+        rotated_img, rotated_img_title = rotate(img)
+        rotated_img_cnt = find_contours(rotated_img)[0]
+
+        return rotated_img_cnt, rotated_img_title
+
     if translate_and_scale_img:
         translated_img, translated_img_title = translate(img)
         translated_and_scaled_img, scaled_img_title = scale(translated_img)
@@ -86,6 +96,11 @@ def transform(
         translated_and_rotated_img, rotated_img_title = rotate(translated_img)
 
         translated_and_rotated_img_cnt = find_contours(translated_and_rotated_img)[0]
+
+        # # bringing the contour back to the center
+        # translated_and_rotated_img_cnt = translated_and_rotated_img_cnt - np.array(
+        #     [img.shape[0] / 2, img.shape[1] / 2], dtype=np.int32
+        # )
 
         translated_and_rotated_img_title = (
             f"{translated_img_title}\n{rotated_img_title}"
@@ -104,6 +119,12 @@ def transform(
             translated_scaled_and_rotated_img
         )[0]
 
+        # # bringing the contour back to the center
+        # translated_scaled_and_rotated_img_cnt = (
+        #     translated_scaled_and_rotated_img_cnt
+        #     - np.array([img.shape[0] / 2, img.shape[1] / 2], dtype=np.int32)
+        # )
+
         translated_scaled_and_rotated_img_title = (
             f"{translated_img_title}\n{scaled_img_title}\n{rotated_img_title}"
         )
@@ -112,15 +133,3 @@ def transform(
             translated_scaled_and_rotated_img_cnt,
             translated_scaled_and_rotated_img_title,
         )
-
-    if scale_img:
-        scaled_img, scaled_img_title = scale(img)
-        scaled_img_cnt = find_contours(scaled_img)[0]
-
-        return scaled_img_cnt, scaled_img_title
-
-    if rotate_img:
-        rotated_img, rotated_img_title = rotate(img)
-        rotated_img_cnt = find_contours(rotated_img)[0]
-
-        return rotated_img_cnt, rotated_img_title
